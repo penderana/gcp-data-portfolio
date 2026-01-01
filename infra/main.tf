ef971b6dd271762cd9f0395c67d25098736da15a
Carlos <carloslop75473@gmail.com>
2026-01-01 22:27:23 +0100

    Creacion de VPC y Firewall SSH. Creacion de VM comentada

---
+++ infra/main.tf
@@ -0,0 +1,51 @@
+# 1. Configuración del Proveedor
+provider "google" {
+  project = "gcp-data-portfolio-cll" 
+  region  = "europe-southwest1"
+  zone    = "europe-southwest1-a"
+}
+
+# 2. Red Privada (VPC) - Coste 0
+# Esto crea tu propio espacio de red aislado
+resource "google_compute_network" "gcp-data-portfolio-cll-vpc-network" {
+  name                    = "gcp-data-portfolio-cll-network"
+  auto_create_subnetworks = true
+}
+
+# 3. Regla de Firewall para SSH - Coste 0
+# Solo permite que alguien (tú) intente entrar por el puerto 22
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.gcp-data-portfolio-cll-vpc-network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+/* # 4. Máquina Virtual N2 - COMENTADA (PARA NO GENERAR COSTES)
+# Solo la activaremos cuando necesitemos desplegar algo real
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "vaicon-server"
+  machine_type = "n2-standard-2" 
+  zone         = "europe-southwest1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size  = 20
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+      # Asigna una IP pública
+    }
+  }
+}
+*/
