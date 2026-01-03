30277601c7bf42e38fc4ba37060ac64047c1c42e
Carlos <carloslop75473@gmail.com>
2026-01-03 21:21:37 +0100

    fixing zip function

--- infra/modules/flight_ingestion/main.tf
+++ infra/modules/flight_ingestion/main.tf
@@ -28,7 +28,7 @@ resource "google_storage_bucket" "function_bucket" {
 data "archive_file" "function_zip" {
   type        = "zip"
   # Ruta relativa: salimos de modules/flight_ingestion/ y vamos a src/flight-function
-  source_dir  = "${path.module}/../../../.github/workflows/src/flight-function/"
+  source_dir  = "${path.module}/../../../src/flight-function/"
   output_path = "/tmp/function.zip"
 }
 
